04934f55f6061832c963a428662c7d98f8c87ee8
gutovesco <guto.vesco@hotmail.com>
2020-02-10 16:45:42 -0300

    styles

--- lib/home.dart
+++ lib/home.dart
@@ -5,18 +5,18 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
           body: Container(
             child: SingleChildScrollView(
       child: Column(children: <Widget>[
         Container(
           width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height / 5.5,
+          height: MediaQuery.of(context).size.height / 4.5,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Spacer(),
-              SizedBox(height: 30),
-              Spacer(),
+
               Center(
                   child: Text(
                     'EasyInvest',
@@ -63,7 +63,48 @@ class Home extends StatelessWidget {
                 ),
               ),
 
-               FlatButton(
+               Container(
+                 width: 50,
+                height: 100,
+                 child: Icon(Icons.arrow_forward, size: 20, color: Colors.grey[800]),
+                 decoration: BoxDecoration(
+                   color: Colors.grey[300],
+                   shape: BoxShape.circle,
+                   boxShadow: [
+                     BoxShadow(
+                       color: Colors.grey[600],
+                       offset: Offset(2.0,2.0),
+                       blurRadius: 12.0,
+                       spreadRadius: 2.0
+                     ),
+                     BoxShadow(
+                       color: Colors.white,
+                       offset: Offset(-4.0,-4.0),
+                       blurRadius: 15.0,
+                       spreadRadius: 1.0
+                     ),
+                   ],
+
+                   gradient: LinearGradient(
+                     begin: Alignment.topLeft,
+                     end: Alignment.bottomRight,
+                     colors: [
+                       Colors.grey[200],
+                       Colors.grey[300],
+                       Colors.grey[400],
+                       Colors.grey[500],
+                     ],
+                     stops: [
+                       0.1,
+                       0.3,
+                       0.8,
+                       0.9,
+                     ]
+                   )
+                 ),
+                 )
+
+               /*FlatButton(
                  hoverColor: Colors.amber,
                  color: Colors.amberAccent,
                         child: Text(
@@ -75,6 +116,6 @@ class Home extends StatelessWidget {
                         ),
                         onPressed: () {
                           Navigator.pop(context);
-                        })
+                        })*/
 
             ]))));}}
